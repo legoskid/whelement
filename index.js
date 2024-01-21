edc3c03272f69aaa1e23cd5ce04bdd61137fdcea
legoskid <legoskid@DESKTOP-7LS9J46>
2024-01-20 19:15:04 -0800

    accept me

---
+++ index.js
@@ -0,0 +1,10 @@
+const axios = require("axios");
+axios.get("http://whale.x10.mx/whale")
+    .then(res1 => {
+        console.log(res1.data)
+    })
+console.log("WHALE ALERT!!!!");
+console.log('\x1b[36m%s\x1b[0m', 'WHALE ALERT!!!!');
+console.log('\x1b[34m%s\x1b[0m', 'WHALE ALERT!!!!');
+console.log("By the way guys, Sh1mmer worked by setting check_enrollment to 0 in the VPD");
+console.log("I have passed the whelement test. Now what's going to happen is the whale alert");
